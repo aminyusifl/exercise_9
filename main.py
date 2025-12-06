720195ce462ae44b03bf58b76c24dc0291174bbd
Amin <yusifliamin410@gmail.com>
2025-12-07 00:19:54 +0400

    btc

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+def get_coin_price(symbol):
+    symbol = symbol.upper()
+    url = f"https://api.binance.com/api/v3/ticker/24hr?symbol={symbol}"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return "False coin name or API is blocked"
+
+    data = response.json()
+    try:
+        return data["lastPrice"]
+    except KeyError:
+        return "Cannot find coin."
+
+coin = input("Enter the coin symbol: ")
+result = get_coin_price(coin)
+print("\nResult: ", result)
